64990811c8922a947d8134cd34a2afa08642e702
runjerry <qinxun.bai@gmail.com>
2022-05-25 21:40:27 -0700

    update dirichlet_infinite to batched envs and agents

--- dirichlet_infinite.py
+++ dirichlet_infinite.py
@@ -1,156 +1,239 @@
 import numpy as np
+import random
 import itertools
+import torch
 
-class DirichletInfiniteAgent:
-    def __init__(self, num_agents, S, A, T, p, trans_p, reward):
+_device_ddtype_tensor_map = {
+    'cpu': {
+        torch.float32: torch.FloatTensor,
+        torch.float64: torch.DoubleTensor,
+        torch.float16: torch.HalfTensor,
+        torch.uint8: torch.ByteTensor,
+        torch.int8: torch.CharTensor,
+        torch.int16: torch.ShortTensor,
+        torch.int32: torch.IntTensor,
+        torch.int64: torch.LongTensor,
+        torch.bool: torch.BoolTensor,
+    },
+    'cuda': {
+        torch.float32: torch.cuda.FloatTensor,
+        torch.float64: torch.cuda.DoubleTensor,
+        torch.float16: torch.cuda.HalfTensor,
+        torch.uint8: torch.cuda.ByteTensor,
+        torch.int8: torch.cuda.CharTensor,
+        torch.int16: torch.cuda.ShortTensor,
+        torch.int32: torch.cuda.IntTensor,
+        torch.int64: torch.cuda.LongTensor,
+        torch.bool: torch.cuda.BoolTensor,
+    }
+}
+
+
+def policy_from_mdp(trans_prob, reward, S, A, tol=0.01):
+    # performs undiscounted value iteration to output an optimal policy
+    value_func = torch.zeros(S)
+    policy = torch.zeros(S)
+    iter = 0
+    gamma = 0.99
+    while True:
+        diff = torch.zeros(S)
+        value = value_func
+        action_return = reward + gamma * torch.einsum('ijk,k->ij', trans_prob, value_func)  # [S, A]
+        value_func, policy = torch.max(action_return, dim=-1)  # [S]
+        diff = torch.maximum(diff, torch.abs(value - value_func))  # [S]
+
+        if torch.any(diff.max() <= tol) or iter >= 10000:
+            break
+
+    return policy  # [S]
+
+
+class DirichletFiniteAgent:
+    def __init__(self, num_agents, num_envs, S, A, trans_p, rewards, optimal_policy):
         """
-        S: state space
-        A: action space
-        s_1: starting state
-        T: time horizon
-        p: parameter between (0, 1]
+        num_agents: number of agents
+        num_envs: number of envs
+        S: size of the state space
+        A: size of the action space
+        trans_p: transitions probabilities, size [n_env, n_agents, n_S, n_A, n_S]
+        rewards: rewards, size [n_envs, n_agents, n_S, n_A]
+        optimal_policy: optimal policies for all envs, size [n_envs, n_S]
         """
         self.num_agents = num_agents
+        self.num_envs = num_envs
         self.S = S
         self.A = A
-        self.T = T
-        self.phi = int(np.floor(S * np.log(S * A / p)))
-        self.w = np.log(T / p)
-        self.k = np.log(T / p)
-        # self.M = np.full([S, A, S, num_agents], self.w)
-        self.M = np.zeros([S, A, S, num_agents])
-        for i in range(num_agents):
-            for s, a in itertools.product(range(S), range(A)):
-                self.M[s, a, :, i] = np.random.dirichlet(np.ones((S)))
+        self.trans_p = trans_p  # [n_envs, n_agents, n_S, n_A, n_S]
+        self.optimal_trans_p = trans_p[:, 0, ...]  # [n_envs, n_S, n_A, n_S]
+        self.rewards = rewards  # [n_envs, n_agents, n_S, n_A]
+        self.optimal_rewards = rewards[:, 0, ...]  # [n_envs, n_S, n_A]
+        self.optimal_policy = optimal_policy  # [n_envs, n_S]
+        # concentration parameters of Dirichlet posterior of transition_p
+        self.alpha = torch.ones(num_envs, S, A, S)  
+        self.reward_mean = torch.zeros(num_envs, S, A) 
+        self.reward_scale = torch.ones(num_envs, S, A)
 
-        self.eta = np.sqrt(T * S / A) + 12 * self.w * S ** 4
-        self.trans_p = trans_p
-        self.reward = reward
+    def posterior_sample(self, alpha, n_sample):
+        dist_trans_p = torch.distributions.dirichlet.Dirichlet(alpha)
+        return dist_trans_p.sample([n_sample])
+        # return torch.transpose(trans_p, 0, 1)
 
-    def posterior_sample(self, transition_prob, M, S, A):
-        dirichlet_trans_p = np.zeros(transition_prob.shape)
-        for s, a in itertools.product(range(S), range(A)):
-            dirichlet_trans_p[s, a] = np.random.dirichlet(M[s, a, :])
-        return dirichlet_trans_p
+    def train(self, num_time_step):
+        cum_regret = torch.zeros(self.num_envs, self.num_agents)
+        num_visits = torch.zeros((self.num_envs, self.S, self.A, self.S))
+        model_reward = torch.zeros((self.num_envs, self.S, self.A))
+        # initialize state tracking for each agent in each env
+        curr_states = torch.randint(0, self.S, size=[self.num_envs, self.num_agents])
+        # initialize state tracking for the optimal agent in each env
+        curr_optimal_state = torch.randint(0, self.S, size=[self.num_envs])
 
-    def compute_policy(self, trans_prob, S, A, phi, reward):
-        # performs undiscounted value iteration to output an optimal policy
-        value_func = np.zeros(S)
-        policy = np.zeros(S)
-        iter = 0
-        gamma = 0.99
-        while True:
-            tolerance = 0.01
-            diff = np.zeros(S)
-            for s in range(S):
-                value = value_func[s]
-                action_returns = []
-                for a in range(A):
-                    action_return = np.sum(
-                        [trans_prob[s, a, s_next] * (reward[s, a, s_next] + gamma * value_func[s_next]) for s_next in range(S)])  # computes the undiscounted returns
-                    action_returns.append(action_return)
-                value_func[s] = np.max(action_returns)
-                policy[s] = np.argmax(action_returns)
-                diff[s] = max(diff[s], np.abs(value - value_func[s]))
-            iter += 1
-            if iter % 10000 == 0:
-                print("diff: ", diff)
-            if np.max(diff) <= tolerance:
-                # print(value_func)
-                break
+        time_step = 1
+        policy = torch.zeros((self.num_envs, self.num_agents, self.S), dtype=torch.int64)
+        ref_num_visits = num_visits
+        while time_step < num_time_step:
+            for env in range(self.num_envs):
+                # check whether to start new epoch for this env 
+                # skip the case when ref_num_visits is 0
+                if torch.any(
+                    (num_visits[env] + 0.1 - 2 * ref_num_visits[env]) * ref_num_visits[env] > 0):
+                    ref_num_visits[env] = num_visits[env]
+                    # resample trans_p and update policy for this env
+                    # [n_agents, n_S, n_A, n_S]
+                    sampled_trans_p = self.posterior_sample(self.alpha[env], self.num_agents)
+                    # extract optimal policies from sampled MDP's: [n_envs, n_agents, n_S]
+                    for agent in range(self.num_agents):
+                        policy[env, agent, :] = policy_from_mdp(
+                            sampled_trans_p[agent], self.rewards[env, agent], self.S, self.A)
 
-        # after value iteration output a deterministic policy
-        # policy = np.zeros(S)
-        # for s in range(S):
-        #     action_returns = []
-        #     for a in range(A):
-        #         action_return = np.sum([trans_prob[s, a, s_next] * (reward[s, a, s_next] + value_func[s_next]) for s_next in range(S)])  # computes the undiscounted returns
-        #         action_returns.append(action_return)
-        #     policy[s] = np.argmax(action_returns) ##doubled actions
-        return policy
+            # agents rollout
+            s_t = curr_states  # [n_envs, n_agents]
+            a_t = policy[
+                torch.arange(self.num_envs).unsqueeze(-1).unsqueeze(-1),
+                torch.arange(self.num_agents).unsqueeze(0).unsqueeze(-1),
+                s_t.unsqueeze(-1)].squeeze()  # [n_envs, n_agents]
+            trans_p = self.trans_p[
+                torch.arange(self.num_envs).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1), 
+                torch.arange(self.num_agents).unsqueeze(0).unsqueeze(-1).unsqueeze(-1), 
+                s_t.unsqueeze(-1).unsqueeze(-1), 
+                a_t.unsqueeze(-1).unsqueeze(-1)].squeeze()  # [n_envs, n_agents, n_S]
+            # [n_envs, n_agents]
+            s_next = torch.distributions.categorical.Categorical(trans_p).sample()
+            curr_states = s_next
 
-    def train(self, epochs, s_t):
-        phi = self.phi
-        w = self.w
-        k = self.k
-        M = self.M
-        T = self.T
+            # optimal agent rollout
+            optimal_s_t = curr_optimal_state
+            optimal_a_t = self.optimal_policy[
+                torch.arange(self.num_envs).unsqueeze(-1),
+                optimal_s_t.unsqueeze(-1)].squeeze()  # [n_envs]
+            optimal_trans_p = self.optimal_trans_p[
+                torch.arange(self.num_envs).unsqueeze(-1).unsqueeze(-1), 
+                optimal_s_t.unsqueeze(-1).unsqueeze(-1), 
+                optimal_a_t.unsqueeze(-1).unsqueeze(-1)].squeeze()  # [n_envs, n_S]
+            optimal_s_next = torch.distributions.categorical.Categorical(
+                optimal_trans_p).sample()  # [n_envs]
+            curr_optimal_state = optimal_s_next
 
-        num_visits = np.zeros((self.S, self.A, self.S, self.num_agents))
-        curr_states = np.zeros((self.num_agents), dtype=np.int)
-        for i in range(len(curr_states)):
-            curr_states[i] = int(s_t)
-        t = 0
+            # collect rewards and update cum_regret
+            reward = self.rewards[
+                torch.arange(self.num_envs).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1), 
+                torch.arange(self.num_agents).unsqueeze(0).unsqueeze(-1).unsqueeze(-1), 
+                s_t.unsqueeze(-1).unsqueeze(-1), 
+                a_t.unsqueeze(-1).unsqueeze(-1)].squeeze()  # [n_envs, n_agents]
+            optimal_reward = self.optimal_rewards[
+                torch.arange(self.num_envs).unsqueeze(-1).unsqueeze(-1), 
+                optimal_s_t.unsqueeze(-1).unsqueeze(-1), 
+                optimal_a_t.unsqueeze(-1).unsqueeze(-1)].squeeze()  # [n_envs]
 
-        cumulative_reward = np.zeros(self.num_agents)
-        max_reward = np.zeros(self.num_agents)
-        for i in range(epochs):
-            num_visits = np.sum(num_visits[:, :, :, :], axis=-1)  # TODO: check if sum is correct
-            num_visits = np.expand_dims(num_visits, axis=-1).repeat(repeats=self.num_agents, axis=-1)
+            cum_regret += optimal_reward.unsqueeze(-1) - reward
 
-            policies = []
+            # record observed transitions and rewards
             for agent in range(self.num_agents):
-                trans_prob = self.posterior_sample(self.trans_p, M[:, :, :, agent], self.S, self.A,)
-                policy = self.compute_policy(trans_prob, self.S, self.A, phi, self.reward)  # computes the max gain policy
-                policies += [policy]
+                num_visits[
+                    torch.arange(self.num_envs).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1),
+                    s_t[:, agent].unsqueeze(-1).unsqueeze(-1).unsqueeze(-1),
+                    a_t[:, agent].unsqueeze(-1).unsqueeze(-1).unsqueeze(-1),
+                    s_next[:, agent].unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)] += 1
+                model_reward[
+                    torch.arange(self.num_envs).unsqueeze(-1).unsqueeze(-1),
+                    s_t[:, agent].unsqueeze(-1).unsqueeze(-1),
+                    a_t[:, agent].unsqueeze(-1).unsqueeze(-1)] \
+                        += reward[:, agent].unsqueeze(-1).unsqueeze(-1)
 
-            num_visits_next = np.copy(num_visits)
-            while True:
-                end_epoch = False
-                for agent in range(self.num_agents):
-                    s_t = curr_states[agent]
-                    a_t = int(policies[agent][s_t])
-                    s_next = np.random.choice(range(0,self.S), size=1, p=self.trans_p[s_t, a_t, :])
-                    # s_next = np.argmax(self.trans_p[s_t, a_t, :]) #should be np.choice
-                    cumulative_reward[agent] += reward[s_t, a_t, s_next]
-                    max_reward[agent] += np.amax(reward[s_t, :, :])  # should do expectation of max reward over transition probability?
-                    num_visits_next[s_t, a_t, s_next, agent] += 1
 
-                    if np.sum(num_visits_next[s_t, a_t, :, agent]) >= 2 * np.sum(num_visits[s_t, a_t, :, agent]):
-                        end_epoch = True
-                    curr_states[agent] = s_next
+            # update the posterior of the Dirichlet alpha of transitions
+            self.alpha = torch.ones(self.alpha.shape) + num_visits
+            # update the posterior of the Gaussian of rewards, [n_envs, n_S, n_A]
+            count = torch.ones(self.num_envs, self.S, self.A) + torch.sum(num_visits, dim=-1)
+            self.reward_mean = model_reward / count
+            self.reward_scale = 1 / torch.sqrt(count)
 
-                t += 1
-                # if t % 500 == 0:
-                #     regret = np.mean(max_reward - cumulative_reward) / t
-                #     print("process at timestep", str(t) + ": " + str(regret))
-                if t == T:
-                    regret = np.mean(max_reward - cumulative_reward) / t
-                    print("regret at time step", str(t) + ": " + str(regret))
-                    return regret
-                    break
+            time_step += 1
 
-                if end_epoch:
-                    M = np.maximum(np.ones(num_visits_next.shape), num_visits_next)
-                    num_visits = num_visits_next
-                    break
-            if t == T:
-                break
+        # evaluate episodic regret
+        per_step_regret = cum_regret / num_time_step  # [n_envs, n_agents]
+        per_step_per_agent_regret = per_step_regret / self.num_agents  # [n_envs]
+        per_step_per_agent_Bayesian_regret = per_step_per_agent_regret.mean()
+        print("bayesian regret: ", per_step_per_agent_Bayesian_regret)
+
+        return per_step_per_agent_Bayesian_regret
+
 
 
 if __name__ == "__main__":
-    #Define MDP
-    state = 5
-    action = 5
-    seeds = np.arange(200, 230)
+    if torch.cuda.is_available():
+        torch.set_default_tensor_type(
+            _device_ddtype_tensor_map['cuda'][torch.get_default_dtype()])
+        torch.cuda.set_device(0)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.use_deterministic_algorithms = True
+
+    num_states = 10
+    num_actions = 5
+    num_envs = 10
+    num_time_step = 200
+    # seeds = range(100, 101)
+    seeds = (100,)
     for seed in seeds:
+        # deterministic settings for current seed
+        random.seed(seed)
         np.random.seed(seed)
-        reward = np.random.normal(0.0, 1.0, size=(state, action, state))
-        trans_p = np.zeros([state, action, state])
-        for i in range(state):
-            for j in range(action):
-                sample = np.random.gamma(1, 1, state)
-                trans_p[i, j, :] = sample / np.sum(sample)
-    #end Define MDP
+        torch.random.manual_seed(seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(seed)
+        print("seed: ", seed)
 
-        total_regret = []
+        # define MDP
+        all_env_rewards = torch.abs(torch.randn(num_envs, num_states, num_actions))
+        dist_trans_p = torch.distributions.dirichlet.Dirichlet(torch.ones(num_states))
+        all_env_trans_p = dist_trans_p.sample([num_envs, num_states, num_actions])
 
-        #TODO: add agents to the list
-        num_agents = [160, 170, 180, 190, 200]
-        for i in num_agents:
-            print("agents: ", i)
-            psrl = DirichletInfiniteAgent(i, state, action, 20000, 0.75, trans_p, reward)
-            regret = psrl.train(1000, int(np.random.randint(0, state, 1)))
-            total_regret += [regret]
+        # compute optimal policy for each env
+        all_env_optimal_policy = torch.zeros((num_envs, num_states), dtype=torch.int64)
+        for env in range(num_envs):
+            all_env_optimal_policy[env] = policy_from_mdp(
+                all_env_trans_p[env], all_env_rewards[env], num_states, num_actions, tol=0.001)
 
-        np.savetxt("result_beyond_150_agents_" + str(seed) + ".csv", np.column_stack((num_agents, total_regret)), delimiter=",")
+        regrets = []
+
+        # TODO: there is a bug for num_agents = 1 
+        list_num_agents = [2, 4, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        # list_num_agents = [80, 90, 100]
+        for num_agents in list_num_agents:
+            print("num of agents: ", num_agents)
+            # [n_envs, n_agents, n_S, n_A, n_S]
+            all_env_agent_rewards = all_env_rewards.unsqueeze(1).repeat(1, num_agents, 1, 1)
+            all_env_agent_trans_p = all_env_trans_p.unsqueeze(1).repeat(1, num_agents, 1, 1, 1)
+            psrl = DirichletFiniteAgent(
+                num_agents, num_envs, num_states, num_actions, 
+                all_env_agent_trans_p, all_env_agent_rewards, all_env_optimal_policy)
+            regret = psrl.train(num_time_step)
+            regrets.append(regret)
+
+        total_regret = torch.stack(regrets)
+        total_regret_np = total_regret.cpu().detach().numpy()
+
+        np.savetxt("results/infinite_" + str(seed) + "_agents_" + str(list_num_agents[-1]) + ".csv", np.column_stack((list_num_agents, total_regret_np)), delimiter=",")
+
+
